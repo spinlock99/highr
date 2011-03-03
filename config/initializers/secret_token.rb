e675ed955109941d6a5cc53da9e63dc5621245cc
Andrew Dixon <dixon@atomicbroadcast.net>
2011-03-02 17:48:27 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Highr::Application.config.secret_token = 'bb96add519dbbc870c9758e6e6fc070d3124af53360e890bf953145aabc2bd631fb5f4fc914fffe7b48c1d0455a8c6e1a19c67e5ac6e88e4372286a45a5705ed'
