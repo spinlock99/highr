609ec0147dce162e727c950abd77e856a6a87d8a
Andrew Dixon <dixon@atomicbroadcast.net>
2011-03-03 18:22:09 -0800

    Removed default rails index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Highr::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  #specify the routes for the static pages
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+
+  #set the root of the application to /pages/home
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
