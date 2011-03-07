7344bbe5c7f7a0b1ccce6213efa7c435caed9f7d
Andrew Dixon <dixon@atomicbroadcast.net>
2011-03-07 10:22:14 -0800

    replaced static images with dynaic ruby image_tag function calls.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,13 @@ module ApplicationHelper
     logo = image_tag("logo.png", :alt => "HighR", :class => "round")
   end
 
+  #Define a "homepage_logo" helper function
+  def home_logo
+    home_logo = image_tag("home_logo.gif", 
+                          :alt => "Homepage Logo", 
+                          :class => "round")
+  end
+  
   #Return a title on a per-page basis.
   def title
     base_title = "HighR"
