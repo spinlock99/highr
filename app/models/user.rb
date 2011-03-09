144ae97380c9985a2ddaa31b9b16bae709086207
Andrew Dixon <dixon@atomicbroadcast.net>
2011-03-09 10:08:10 -0800

    Added rake task - db:populate - to populate the development database with fake users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 # == Schema Information
-# Schema version: 20110304165246
+# Schema version: 20110307054423
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 #User model is a subclass of ActiveRecord::Base 
