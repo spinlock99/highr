24f9e696c715a80409ec01e55fe760b7ec926ce0
Andrew Dixon <dixon@atomicbroadcast.net>
2011-04-19 15:56:44 -0700

    configured production mail server.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,28 @@ Highr::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { 
+    :host => 'atomicbroadcast.heroku.com' 
+  }
+
+  # Do care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+
+  # set-up action_mailer to send emails through mail.atomicbroadcast.net
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+#  require 'tlsmail'
+  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+  config.action_mailer.smtp_settings = {
+    :address => "mail.atomicbroadcast.net",
+    :port => 25,
+    :domain => "atomicbroadcast.net",
+    :user_name => "support@atomicbroadcast.net",
+    :password => "AB!@1110tast",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
