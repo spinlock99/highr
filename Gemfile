60a9ba90b6af0a18cac7ea266ab3ee90b8082e44
Andrew Dixon <dixon@atomicbroadcast.net>
2011-06-03 15:45:26 -0700

    Added devise_invitable.

--- Gemfile
+++ Gemfile
@@ -5,13 +5,13 @@ gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
 gem 'gravatar_image_tag', '1.0.0.pre2'
 gem 'will_paginate', '3.0.pre2'
 gem 'devise', '1.2.1'
+gem 'devise_invitable', '~> 0.4.0'
 gem 'nifty-generators', '0.4.6'
 gem 'omniauth', '0.2.0'
 gem 'multi_json', '0.0.5'
 gem 'linkedin', '0.2.2'
 gem 'crack', '0.1.8'
 gem 'jquery-rails', '0.2.7'
-
 gem 'tlsmail', '0.0.1'
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110411220555) do
+ActiveRecord::Schema.define(:version => 20110603221452) do
 
   create_table "access_tokens", :force => true do |t|
     t.integer  "user_id"
@@ -161,7 +161,7 @@ ActiveRecord::Schema.define(:version => 20110411220555) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => ""
     t.string   "reset_password_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                       :default => 0
@@ -174,9 +174,16 @@ ActiveRecord::Schema.define(:version => 20110411220555) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "picture_url"
+    t.string   "invitation_token",     :limit => 60
+    t.datetime "invitation_sent_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["invitation_token"], :name => "index_users_on_invitation_token"
+  add_index "users", ["invited_by_id"], :name => "index_users_on_invited_by_id"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
