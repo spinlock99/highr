040a7c01bc9f2dad67f00f4c8e3f2fbf20d405c1
Andrew Dixon <dixon@atomicbroadcast.net>
2011-06-08 14:50:43 -0700

    Fixed user integration spec so that it works with Devise.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,19 +11,15 @@ describe "Users" do
 
       it "should not make a new user" do
         lambda do
-          #visit the home page
-          visit root_path
+          #visit the sign up page
+          visit new_user_registration_path
           #fill in bad info in the fields
-          within "#sign_up" do
-            fill_in "user[email]", :with => ""
-            fill_in "user[password]", :with => ""
-            fill_in "user[password_confirmation]", :with => ""
-            #push the button
-            click_button "Sign Up"
-          end
+          fill_in :email, :with => ""
+          fill_in :password, :with => ""
+          fill_in :password_confirmation, :with => ""
+          #push the button
+          click_button "Sign up"
           #validate response
-#          response.should render_template('registrations/new')
-#          response.should have_selector("div#error_explanation")
         end.should_not change(User, :count)
       end
    
@@ -34,40 +30,15 @@ describe "Users" do
           #fill in bad info in the fields
           fill_in "Email", :with => ""
           fill_in "Password", :with => ""
-#          fill_in "user_password_confirmation]", :with => ""
           fill_in "Confirmation", :with => ""
           #push the button
           click_button "Sign Up"
           #validate response
- #         response.should render_template('users/new')
- #         response.should have_selector("div#error_explanation")
         end.should_not change(User, :count)
       end
     end # describe "failure"
 
     describe "success" do
-
-#      it "should make a new user on homepage" do
-#        lambda do
-#          #go to the home page 
-#          visit root_path
-#          #fill in good data in the form
-#          within '#sign_up' do
-#            fill_in "user[username]", :with => "Example User"
-#            fill_in "user[email]", :with => "user@example.com"
-#            fill_in "user[password]", :with => "foobar"
-#            fill_in "user[password_confirmation]", :with => "foobar"
-#            #push the button
-#            click_button
-#          end
-#          #check the response
-#          response.should have_selector("div.flash.success",
-#                                        :content => "Welcome")
-#          response.should render_template('users/show')
-#          #now make sure that we have incremented the number of users
-#          #in the database.
-#          end.should change(User, :count).by(1)
-#      end
 
       it "should make a new user" do
         lambda do
@@ -79,12 +50,8 @@ describe "Users" do
           fill_in "Confirmation", :with => "foobar"
           #push the button
           click_button "Sign Up"
-#          save_and_open_page
           #check the response
           page.has_selector?('title', :text => 'Home').should be_true
-#          response.should have_selector("div.flash.success",
-#                                        :content => "Welcome")
-#          response.should render_template('pages/home')
         #now make sure that we have incremented the number of users
         #in the database.  
         end.should change(User, :count).by(1)
@@ -101,7 +68,6 @@ describe "Users" do
         fill_in :password, :with => ""
         click_button "Sign in"
         page.should have_selector("div.flash.alert", :content => "Invalid")
-#        response.should have_selector("div.flash.alert", :content => "Invalid")
       end
     end #describe "failure"
 
