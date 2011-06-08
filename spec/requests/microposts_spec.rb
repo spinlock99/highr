191cf5b4d27db54782b840fba2b42a15b544ad25
Andrew Dixon <dixon@atomicbroadcast.net>
2011-06-08 13:58:29 -0700

    Fixed micropost integration test. Micropost functionality was moved from root_path to team_path(@team)

--- spec/requests/microposts_spec.rb
+++ spec/requests/microposts_spec.rb
@@ -3,14 +3,11 @@ require 'spec_helper'
 describe "Microposts" do
 
   before(:each) do
-    user = Factory(:user)
-    user.save!
-#      visit new_user_session_path
-#      fill_in "Email", :with => user.email
-#      fill_in "Password", :with => user.password
-#      click_button "Sign in"
-#      save_and_open_page
-    integration_sign_in user
+    @user = Factory(:user)
+    @team = Factory(:team)
+    @user.save!
+    integration_sign_in @user
+    @user.join!(@team)
   end
 
   describe "creation" do
@@ -19,7 +16,7 @@ describe "Microposts" do
       
       it "should not make a new micropost" do
         lambda do
-          visit root_path
+          visit team_path(@team)
           fill_in "micropost_content", :with => ""
           click_button "Submit"
         end.should_not change(Micropost, :count)
@@ -32,10 +29,9 @@ describe "Microposts" do
       it "should make a new micropost" do
         content = "Mr. T says: sucka"
         lambda do
-          visit root_path
+          visit team_path(@team)
           fill_in "micropost_content", :with => content
           click_button "Submit"
-#          save_and_open_page
           page.should have_content("Micropost created")
         end.should change(Micropost, :count).by(1)
       end
