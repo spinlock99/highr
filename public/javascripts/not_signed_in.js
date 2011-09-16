21513129d569ef7d2c006c2c6c8a22ca1d0775a2
Andrew Dixon <dixon@atomicbroadcast.net>
2011-09-16 09:54:46 -0700

    Friday morning commit

--- public/javascripts/not_signed_in.js
+++ public/javascripts/not_signed_in.js
@@ -1,34 +1,45 @@
 // initial text in email field on landing page form
 $(document).ready(function(){
-	//
-	// Show closed curtains to non-signed in users
-	//
-	$('#content').css('opacity', '0.8')
-	$('#container').before('<div class="curtain" id="left"></div>' +
-			       '<div class="back-drop" id="center-left"></div>' +
-			       '<div class="back-drop" id="center-right"></div>' +
-			       '<div class="curtain" id="right"></div>');
-	$('.curtain').css('float', 'left').css('width','50%').css('height','915px');
-	$('.back-drop').css('float', 'left').css('width','0%').css('height','915px');
-	$('#right').css('background-position','-100px','0px');
-	//
-	// setup example email address
-	//
-	var initialText = "your@email.com";
-        $('#email').focus(function(){
-		if( $(this).val()==initialText){
-		    $(this).val("").removeClass("initial");
-		}
-	    });
-	$('#email').blur(function(){
-		if($(this).val()==""){
-		    $(this).val(initialText).addClass("initial");
-		}
-	    });
-	//
-	// AJAX call to submit user_new form
-	//
-	$("#user_new").submitWithAjax();
-	$("#user_submit").submitWithAjax();
-	$("#commit").submitWithAjax();
+    //
+    // Show closed curtains to non-signed in users
+    //
+    $('#content').css('opacity', '0.8')
+    $('#container').before('<div class="curtain" id="left"></div>' +
+			   '<div class="back-drop" id="center-left"></div>' +
+			   '<div class="back-drop" id="center-right"></div>' +
+			   '<div class="curtain" id="right"></div>');
+    $('.curtain').css('float', 'left').css('width','50%').css('height','915px');
+    $('.back-drop').css('float', 'left').css('width','0%').css('height','915px');
+    $('#right').css('background-position','-100px','0px');
+    //
+    // setup example email address
+    //
+    var initialText = "your@email.com";
+    $('#email').focus(function(){
+	if( $(this).val()==initialText){
+	    $(this).val("").removeClass("initial");
+	}
     });
+    $('#email').blur(function(){
+	if($(this).val()==""){
+	    $(this).val(initialText).addClass("initial");
+	}
+    });
+    //
+    // AJAX call to submit user_new form
+    //
+    $("#user_new").submitWithAjax();
+    $("#user_submit").submitWithAjax();
+    $("#commit").submitWithAjax();
+});
+ 
+document.onclick = openCurtains;
+
+function openCurtains() {
+    var waitTime = 2000;
+
+    $("#left").animate({ width: '25%' }, waitTime);
+    $("#center-left").animate({ width: '25%' }, waitTime);
+    $("#center-right").animate({ width: '25%' }, waitTime);
+    $("#right").animate({ width: '25%' }, waitTime);  
+}
--- public/javascripts/signed_in.js
+++ public/javascripts/signed_in.js
@@ -2,51 +2,51 @@
 // initial text in email field on landing page form
 //
 $(document).ready(function(){
-	var initialText = "friends@email.com";
-        $('#email').focus(function(){
-		if( $(this).val()==initialText){
-		    $(this).val("").removeClass("initial");
-		}
-	    });
-	$('#email').blur(function(){
-		if($(this).val()==""){
-		    $(this).val(initialText).addClass("initial");
-		}
-	    });
-	//
-	// facebook button
-	//
-	likebutton = 
-	    '<fb:like href="" send="" width="100" show_faces="" ' +
-	    'font="" layout="button_count" action="recommend">' +
-	    '</fb:like>';
-	$.getScript('http://connect.facebook.net/en_US/all.js', function() {
-		FB.init({appId: 141936272547391, 
-			    status: true, 
-			    cookie: true, 
-			    xfbml: true
-			    });
-		$('#facebook').replaceWith(likebutton);
-	    });
-	//
-	// twitter button
-	//
-	tweetbutton = 
-	    '<a href="http://twitter.com/share" class="twitter-share-button"' +
-	    ' data-text="Atomic Broadcast is the next big thing!"' +
-	    ' data-count="horizontal">Tweet</a>';
-	
-	$('#twitter').replaceWith(tweetbutton);
-	$.getScript('http://platform.twitter.com/widgets.js', function(){
-		return true;
-	    });
-	//
-	// Open the Curtains
-	//
-	var waitTime = 5000;
-	$("#content").delay(waitTime).animate({ opacity: '1.0' }, waitTime);
-	$("#left").delay(waitTime).animate({ width: '25%' }, waitTime);
-	$("#center-left").delay(waitTime).animate({ width: '25%' }, waitTime);
-	$("#center-right").delay(waitTime).animate({ width: '25%' }, waitTime);
-	$("#right").delay(waitTime).animate({ width: '25%' }, waitTime);
+    var initialText = "friends@email.com";
+    $('#email').focus(function(){
+	if( $(this).val()==initialText){
+	    $(this).val("").removeClass("initial");
+	}
     });
+    $('#email').blur(function(){
+	if($(this).val()==""){
+	    $(this).val(initialText).addClass("initial");
+	}
+    });
+    //
+    // facebook button
+    //
+    likebutton = 
+	'<fb:like href="" send="" width="100" show_faces="" ' +
+	'font="" layout="button_count" action="recommend">' +
+	'</fb:like>';
+    $.getScript('http://connect.facebook.net/en_US/all.js', function() {
+	FB.init({appId: 141936272547391, 
+		 status: true, 
+		 cookie: true, 
+		 xfbml: true
+		});
+	$('#facebook').replaceWith(likebutton);
+    });
+    //
+    // twitter button
+    //
+    tweetbutton = 
+	'<a href="http://twitter.com/share" class="twitter-share-button"' +
+	' data-text="Atomic Broadcast is the next big thing!"' +
+	' data-count="horizontal">Tweet</a>';
+    
+    $('#twitter').replaceWith(tweetbutton);
+    $.getScript('http://platform.twitter.com/widgets.js', function(){
+	return true;
+    });
+    //
+    // Open the Curtains
+    //
+    var waitTime = 5000;
+    $("#content").delay(waitTime).animate({ opacity: '1.0' }, waitTime);
+    $("#left").delay(waitTime).animate({ width: '25%' }, waitTime);
+    $("#center-left").delay(waitTime).animate({ width: '25%' }, waitTime);
+    $("#center-right").delay(waitTime).animate({ width: '25%' }, waitTime);
+    $("#right").delay(waitTime).animate({ width: '25%' }, waitTime);
+});
